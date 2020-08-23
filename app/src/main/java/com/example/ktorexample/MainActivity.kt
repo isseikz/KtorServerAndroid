f0c00087a707c282cbc5a885af48da095ac6d67a
isseikz <mail@isseikuzumaki.tokyo>
2020-08-23 11:13:33 +0900

    Jetty

--- app/src/main/java/com/example/ktorexample/MainActivity.kt
+++ app/src/main/java/com/example/ktorexample/MainActivity.kt
@@ -7,10 +7,23 @@ import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.jetty.Jetty
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.Default).launch {
+            embeddedServer(Jetty, 8080) {
+                routing {
+                    get("/") {
+                        call.respondText("Hello", ContentType.Text.Html)
+                    }
+                }
+            }.start(wait = true)
+        }
     }
 }
