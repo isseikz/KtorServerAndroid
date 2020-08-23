9297fb641ebcacdad0e5dc27b75fd8d193732096
isseikz <mail@isseikuzumaki.tokyo>
2020-08-23 10:46:38 +0900

    core

--- app/src/main/java/com/example/ktorexample/MainActivity.kt
+++ app/src/main/java/com/example/ktorexample/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.ktorexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
